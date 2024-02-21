e698ebb4393d92becde3b551409881f63ed2e9da
Schifler Patricia-Vivien <93488786+Actulus@users.noreply.github.com>
2024-02-22 01:29:16 +0200

    small image caroussel-like thing

--- MauiApp1/MauiApp1/ImageLayoutPage.xaml.cs
+++ MauiApp1/MauiApp1/ImageLayoutPage.xaml.cs
@@ -2,18 +2,36 @@ namespace MauiApp1;
 
 public partial class ImageLayoutPage : ContentPage
 {
-	public ImageLayoutPage()
+    private List<string> imageSources = new List<string>
+        {
+            "https://static.wikia.nocookie.net/universalstudios/images/9/98/Shrek.png/revision/latest?cb=20220728141753",
+            "E:\\Egyetem\\6. felev\\DOTNET\\cat.jpg",
+            "donkey.jpg"
+        };
+    private int currentIndex = 0;
+
+    public ImageLayoutPage()
 	{
 		InitializeComponent();
-	}
+        UpdateImage();
+    }
 
     private void OnBackButtonClicked(object sender, EventArgs e)
     {
-        Navigation.PopAsync();
+        currentIndex = (currentIndex + 1) % imageSources.Count; // Cycle forward
+        UpdateImage();
     }
 
     private void OnNextButtonClicked(object sender, EventArgs e)
     {
-        Navigation.PushAsync(new MainPage());
+        //Navigation.PushAsync(new MainPage()); // go to next page
+        currentIndex = (currentIndex - 1 + imageSources.Count) % imageSources.Count; // Cycle backward
+        UpdateImage();
+    }
+
+    private void UpdateImage()
+    {
+        var currentSource = imageSources[currentIndex];
+        currentImage.Source = currentSource;
     }
 }
