2450400b7fbb2c558a0182987db512f248e901d5
Schifler Patricia-Vivien <93488786+Actulus@users.noreply.github.com>
2024-02-21 16:32:26 +0200

    ex 3

---
+++ MauiApp1/MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+
+        string userName; 
+        string password;
+
+        public MainPage()
+        {
+            userName = "";
+            password = "";
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            //DisplayAlert("title", "Szia " + msg, "cancel");
+        }
+
+         private void OnLoginClicked(object sender, EventArgs e)
+        {
+            DisplayAlert("title", "Login clicked", "cancel");
+        }
+
+        private void OnCloseClicked(object sender, EventArgs e)
+        {
+            DisplayAlert("title", "Close clicked", "cancel");
+        }
+
+        private void UserIdEntry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            userName = e.NewTextValue;
+        }
+
+        private void PasswordEntry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            password = e.NewTextValue;
+        }
+    }
+
+}
