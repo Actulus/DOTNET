e6b7cd98c19bdaf7423ee6005231d89ebae6d0ea
Schifler Patricia-Vivien <93488786+Actulus@users.noreply.github.com>
2024-04-24 15:27:05 +0300

    some basic listing?

---
+++ FirstApp/FirstApp/Controllers/FirstController.cs
@@ -0,0 +1,27 @@
+﻿using FirstApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstApp.Controllers
+{
+    public class FirstController : Controller
+    {
+        public string Index()
+        {
+            return "Hello World";
+        }
+
+        public IActionResult Hello()
+        {
+            return View();
+        }
+
+        public IActionResult Info()
+        {
+            Person person = new Person();
+            person.Name = "John";
+            person.Age = 18;
+            person.Location = "United States";
+            return View(person);
+        }
+    }
+}
---
+++ FirstApp/FirstApp/Models/Person.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace FirstApp.Models
+{
+    public class Person
+    {
+        public string Name { get; set; } = String.Empty;
+        public int Age { get; set; }
+        public string Location { get; set; } = String.Empty;
+    }
+}
