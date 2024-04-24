9272363ed4a351c0c49afee73a3e4e85443e9e0f
Schifler Patricia-Vivien <93488786+Actulus@users.noreply.github.com>
2024-04-24 15:26:44 +0300

    CRUD exercises

---
+++ CRUD/CRUD/Controllers/EmployeeController.cs
@@ -0,0 +1,60 @@
+﻿using CRUD.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRUD.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(Repository.AllEmpoyees);
+        }
+        // HTTP GET VERSION
+        public IActionResult Create()
+        {
+            return View();
+        }
+		// HTTP POST VERSION
+		[HttpPost]
+		public IActionResult Create(Employee employee)
+		{
+			if (ModelState.IsValid)
+			{
+				Repository.Create(employee);
+				return View("Thanks", employee);
+			}
+			else
+				return View();
+		}
+
+		public IActionResult Update(string empname)
+        {
+            Employee employee = Repository.AllEmpoyees.Where(e => e.Name == empname).FirstOrDefault();
+            return View(employee);
+        }
+		[HttpPost]
+		public IActionResult Update(Employee employee, string empname)
+		{
+			if (ModelState.IsValid)
+			{
+				Repository.AllEmpoyees.Where(e => e.Name == empname).FirstOrDefault().Age = employee.Age;
+				Repository.AllEmpoyees.Where(e => e.Name == empname).FirstOrDefault().Salary = employee.Salary;
+				Repository.AllEmpoyees.Where(e => e.Name == empname).FirstOrDefault().Department =
+				employee.Department;
+				Repository.AllEmpoyees.Where(e => e.Name == empname).FirstOrDefault().Sex = employee.Sex;
+				Repository.AllEmpoyees.Where(e => e.Name == empname).FirstOrDefault().Name = employee.Name;
+				return RedirectToAction("Index");
+			}
+			else
+				return View();
+		}
+
+		[HttpPost]
+        public IActionResult Delete(string empname)
+        {
+            Employee employee = Repository.AllEmpoyees.Where(e => e.Name == empname).FirstOrDefault();
+            Repository.Delete(employee);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ CRUD/CRUD/Models/Repository.cs
@@ -0,0 +1,20 @@
+﻿namespace CRUD.Models
+{
+    public static class Repository
+    {
+        private static List<Employee> allEmpoyees = new List<Employee>();
+        public static IEnumerable<Employee> AllEmpoyees
+        {
+            get { return allEmpoyees; }
+        }
+        public static void Create(Employee employee)
+        {
+            allEmpoyees.Add(employee);
+        }
+
+        public static void Delete(Employee employee)
+        {
+            allEmpoyees.Remove(employee);
+        }
+    }
+}
